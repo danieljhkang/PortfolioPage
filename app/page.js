5a2b306af9a5e0745cb9acba6461a807d4e23660
Daniel Kang <dkang.jh@gmail.com>
2024-03-11 23:53:11 -0400

    main component

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import Main from "@/components/Main";
 
 export default function Home() {
   return (
     <div>
       <Head></Head>
       <Navbar />
+      <Main />
     </div>
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -46,7 +46,9 @@ const Navbar = () => {
       </div>
 
       <div
-        className={nav ? "fixed left-0 top-0 w-full h-screen bg-black/70" : ""}
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+        }
       >
         <div
           className={
